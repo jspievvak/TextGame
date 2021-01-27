e61ac2cd4840a9d1032bcfb1a548235a7451cc24
DESKTOP-5PKPSGL\ADMIN <jspievvak@gmail.com>
2021-01-28 00:28:56 +0100

    Text + image added

---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextController : MonoBehaviour
+{
+    public Text text;
+
+    void Start()
+    {
+        text.text   
+        //Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacus lacus, consectetur nec gravida ac, sollicitudin vel tortor. Sed nec leo auctor, pretium elit ut, faucibus ante. In vestibulum ligula ac odio consequat, in rutrum massa volutpat. Cras eget eleifend quam. Sed id lorem sed elit finibus accumsan ac ut lectus. Nullam interdum quam et diam tristique, ut cursus metus tempus. Morbi id elit enim. Donec iaculis et urna vitae volutpat. Vivamus in ex ex
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
