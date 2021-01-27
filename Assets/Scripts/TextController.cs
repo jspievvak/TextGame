3124b88d4b1ddb0bfe984c9e3e65588e881b4dd0
DESKTOP-5PKPSGL\ADMIN <jspievvak@gmail.com>
2021-01-28 00:43:25 +0100

    Added Canvas

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextController : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class TextController : MonoBehaviour
 
     void Start()
     {
-        text.text   
+        text.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacus lacus, consectetur nec gravida ac, sollicitudin vel tortor. Sed nec leo auctor, pretium elit ut, faucibus ante. In vestibulum ligula ac odio consequat, in rutrum massa volutpat. Cras eget eleifend quam. Sed id lorem sed elit finibus accumsan ac ut lectus. Nullam interdum quam et diam tristique, ut cursus metus tempus. Morbi id elit enim. Donec iaculis et urna vitae volutpat. Vivamus in ex ex" ;
         //Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacus lacus, consectetur nec gravida ac, sollicitudin vel tortor. Sed nec leo auctor, pretium elit ut, faucibus ante. In vestibulum ligula ac odio consequat, in rutrum massa volutpat. Cras eget eleifend quam. Sed id lorem sed elit finibus accumsan ac ut lectus. Nullam interdum quam et diam tristique, ut cursus metus tempus. Morbi id elit enim. Donec iaculis et urna vitae volutpat. Vivamus in ex ex
     }
 
